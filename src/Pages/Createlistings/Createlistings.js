e88ac2e041a1eb77277270b2ccce71f952449daf
Exabiier <exabiier.bars@gmail.com>
2022-12-29 18:49:43 -0600

    Made more updates for deployment

--- src/Pages/Createlistings/Createlistings.js
+++ src/Pages/Createlistings/Createlistings.js
@@ -101,33 +101,30 @@ const onSubmit = async (e) =>{
     let geolocation ={};
     let location;
 
-    if(geolocationEnabled){
-        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_API}`)
-
+    if (geolocationEnabled) {
+        const response = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        )
+  
         const data = await response.json()
-
-        // getting the geo-location from 
-        // the Geolocation will not work if the ? are not put in place why. i dont know
-        geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
-
-        geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
-
-        // If the status is Zero result. 
-        location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
-
-        
-
-        // Message for if the adress does not exists
-    if (location === undefined || location.includes('undefined')){
-        setLoading(false);
-        toast.error('Please enter a correct address');
-        return;
-    }
-
-    } else {
-       geolocation.lat = latitude
-       geolocation.lat = longitude
-    }
+  
+        geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+        geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+  
+        location =
+          data.status === 'ZERO_RESULTS'
+            ? undefined
+            : data.results[0]?.formatted_address
+  
+        if (location === undefined || location.includes('undefined')) {
+          setLoading(false)
+          toast.error('Please enter a correct address')
+          return
+        }
+      } else {
+        geolocation.lat = latitude
+        geolocation.lng = longitude
+      }
 
 // /////////////  Store images in fire base  //////////////////
   const storeImage = async (image) => {
